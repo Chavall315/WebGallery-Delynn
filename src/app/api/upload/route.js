5f0b54e1fd2efa24a2e8e1e358da400292181cda
Chavall315 <naufalbagaskara31@gmail.com>
2025-10-17 23:15:46 +0700

    ubah output prisma

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import cloudinary from "@/lib/cloudinary";
-import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
